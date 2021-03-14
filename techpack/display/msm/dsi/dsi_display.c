b079cd109aeed65dfab41bd81ba1420715751f22
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-03-14 14:21:12 +0530

    Revert "adreno_tz: Fix GPU target frequency calculation for high refresh rates"
    This reverts commit 64048ff06039ac0114955916930934b4b6a19987.
    
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- techpack/display/msm/dsi/dsi_display.c
+++ techpack/display/msm/dsi/dsi_display.c
@@ -625,8 +625,6 @@ eTool discrimination_ANA6706_ToolsType(char* szLotID, int WaferNumber)
     return toolType;
 }
 
-static unsigned int cur_refresh_rate = 60;
-
 static void dsi_display_mask_ctrl_error_interrupts(struct dsi_display *display,
 			u32 mask, bool enable)
 {
@@ -8695,11 +8693,6 @@ int dsi_display_pre_commit(void *display,
 	return rc;
 }
 
-unsigned int dsi_panel_get_refresh_rate(void)
-{
-	return READ_ONCE(cur_refresh_rate);
-}
-
 int dsi_display_enable(struct dsi_display *display)
 {
 	int rc = 0;
@@ -8796,7 +8789,6 @@ int dsi_display_enable(struct dsi_display *display)
 	mutex_lock(&display->display_lock);
 
 	mode = display->panel->cur_mode;
-	WRITE_ONCE(cur_refresh_rate, mode->timing.refresh_rate);
 
 	if (mode->dsi_mode_flags & DSI_MODE_FLAG_DMS) {
 		rc = dsi_panel_post_switch(display->panel);
