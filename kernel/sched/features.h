cdfadb27144c76e4c18ef6fd235932ff3f19273a
Prashant Shukla <shuklaprashant847@gmail.com>
2022-09-20 09:09:41 +0000

    Revert "Edit features"
    This reverts commit 87fdac714b0833d9788e5ba0e650f320bf3a9c4c.

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -44,7 +44,7 @@ SCHED_FEAT(LB_BIAS, true)
 /*
  * Decrement CPU capacity based on time not spent running tasks
  */
-SCHED_FEAT(NONTASK_CAPACITY, true)
+SCHED_FEAT(NONTASK_CAPACITY, false)
 
 /*
  * Queue remote wakeups on the target CPU and process them
@@ -78,7 +78,7 @@ SCHED_FEAT(WARN_DOUBLE_CLOCK, false)
 SCHED_FEAT(RT_PUSH_IPI, true)
 #endif
 
-SCHED_FEAT(RT_RUNTIME_SHARE, false)
+SCHED_FEAT(RT_RUNTIME_SHARE, true)
 SCHED_FEAT(LB_MIN, false)
 SCHED_FEAT(ATTACH_AGE_LOAD, true)
 
@@ -105,7 +105,7 @@ SCHED_FEAT(FIND_BEST_TARGET, true)
  *   those tasks through the mainline slow path.
  */
 
-SCHED_FEAT(EAS_PREFER_IDLE, false)
+SCHED_FEAT(EAS_PREFER_IDLE, true)
 
 /*
  * Request max frequency from schedutil whenever a RT task is running.
